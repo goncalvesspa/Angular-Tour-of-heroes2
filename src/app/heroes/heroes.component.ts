1e825c8fe7475d8c92ed9bec1a785032fc8582cf
Paulo Goncalves <paulo.goncalves@iteris.com.br>
2020-02-20 16:06:50 -0300

    inicio da parte 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, /*  OnChanges, Input  */ } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 /* import { from } from 'rxjs';  */
 
 
@@ -11,27 +12,42 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  //heroes = HEROES;
 
-  hero: Hero = {
+  heroes: Hero[];
+
+  /* hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  } */
 
-  trocaName(){
+  //heroText: string = 'teste';
+
+ /*  trocaName(){
     this.hero.name = '';
-  }
+  } */
+
+
+ 
+
 
   selectedHero: Hero;
   onSelect(hero: Hero):void {
     this.selectedHero = hero;
-}
+  }
   //hero = 'Windstorm';
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
 
   ngOnInit() {
-
+    this.getHeroes();
+   //this.selectedHero = { id: 1, name: 'Superman' };
   }
 
   
